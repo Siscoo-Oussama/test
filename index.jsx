8807a4741ccede28277a60cb97f5f161d1aaffb2
oussama-siscoo <oussama.aitboulaha@gmail.com>
2022-06-21 12:52:19 +0100

    first commit

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>hello from index component seconde commit </div>
+    )
+  }
+}
+
+export default index
